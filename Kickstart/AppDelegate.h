dda9ff7c4812974760680378554c4e6ff8d061ca
tinytitan <tinytitan@tinytitans-MacBook-Pro.local>
2015-01-04 19:27:20 +0800

    Initial Commit

---
+++ Kickstart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kickstart
+//
+//  Created by tinytitan on 1/4/15.
+//  Copyright (c) 2015 tinytitan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kickstart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kickstart
+//
+//  Created by tinytitan on 1/4/15.
+//  Copyright (c) 2015 tinytitan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
